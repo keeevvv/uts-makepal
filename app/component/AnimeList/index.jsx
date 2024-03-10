c6fdd4a6e6f91be0197902a5767fe26b008042b0
KevZ12 <135513743+KevZ12@users.noreply.github.com>
2024-03-11 02:37:24 +0700

    responsive home page/top anime

--- app/component/AnimeList/index.jsx
+++ app/component/AnimeList/index.jsx
@@ -18,47 +18,59 @@ const AnimeList = ({
 
   return (
     <div>
-      <div className="flex justify-start bg-gray-300 rounded-md hover:cursor-pointer w-auto h-70 pb-2">
+      <div className="flex justify-start bg-gray-300 rounded-md  w-auto pb-2   ">
         <img
           src={img}
           alt=""
-          className="w-[200px] h-[290px] rounded-md pt-3 pl-3"
+          className=" pt-7 w-[200px] h-[215px] hover:cursor-pointer sm:h lg:w-[200px] lg:h-[290px] rounded-md lg:pt-3 pl-3 md:w-[120px] md:h-[195px] md:pt-5"
         />
 
-        <div className="mt-2 ">
-          <div className="flex gap-2 pl-3">
-            <h4 className="font-semibold">{order + 1}.</h4>
-            <h4 className="font-semibold pb-2">{title}</h4>
+        <div className=" mt-6 lg:mt-2 lg:pt-2 ">
+          <div className="flex lg:gap-2 md:gap-1 pl-3">
+            <h4 className="font-bold text-[12px] lg:text-[16px] md:text-[9px]">
+              {order + 1}.
+            </h4>
+            <h4 className="font-bold pb-2 text-[12px] lg:text-[16px] md:text-[9px] mr-6 hover:cursor-pointer">
+              {title}
+            </h4>
           </div>
 
           <div className="">
             <div>
-              <p className="text-xs font-semibold ml-7">{airing}</p>
+              <p className="text-xs font-semibold ml-7 lg:text-sm md:text-[9px] lg:text-[12px]">
+                {airing}
+              </p>
             </div>
             {type === "TV" ? (
-              <p className="text-xs font-semibold ml-7">
+              <p className="text-xs font-normal ml-7 lg:text-sm md:text-[10px]">
                 TV Series •{episodes} Episodes
               </p>
             ) : (
               // Kondisi jika false
-              <p className="text-xs font-semibold ml-7">Movie •{duration}</p>
+              <p className="text-xs font-semibold ml-7 lg:text-sm md:text-xs">
+                Movie •{duration}
+              </p>
             )}
-            <div className="  overflow-hidden hover:overflow-auto max-h-[175px] pl-7 pr-[30px] ">
-              <p className="text-xs ">{synopsis}</p>
+            <div className="  max-h-[100px] hover:cursor-pointer overflow-hidden hover:overflow-auto lg:max-h-[175px] md:max-h-[90px] pl-7 pr-[30px]  ">
+              <p className="text-xs lg:text-[13px] ">{synopsis}</p>
             </div>
           </div>
 
-          <div className="flex flex-row gap-2 ml-7 mt-1 ">
+          <div className="flex flex-row gap-2 ml-7 mt-1 mr-1">
             {genre.map((data, i) => {
               return (
-                <div key={i} className=" bg-slate-400 rounded-md">
-                  <p className="text-xs p-1">{data.name}</p>
+                <div key={i} className=" bg-slate-400 rounded-md w-13 ">
+                  <p className="text-[8px] font-semibold p-1 hover:cursor-pointer">
+                    {data.name}
+                  </p>
                 </div>
               );
             })}
           </div>
-          <div className="flex ml-64   ">
-            <p className="font-semibold">Score: {score}</p>
+          <div className="flex ml-8   ">
+            <p className="font-semibold md:text-[10px] lg:text-[14px]">
+              Score: {score}
+            </p>
           </div>
         </div>
       </div>
--- app/page.js
+++ app/page.js
@@ -9,22 +9,22 @@ const Home = async () => {
   return (
     <div>
       <h1>Populer</h1>
-      <div className="grid sm:grid-cols-2 grid-cols-1 gap-10 px-10">
+      <div className="grid md:grid-cols-2 grid-cols-1 md:gap-6 q lg:gap-10 lg:px-10 md:px-7 gap-7  ">
         {anime.data.map((data, i) => {
-          
           return (
             <div key={data.mal_id}>
               <AnimeList
+              anime = {anime}
                 title={data.title}
                 img={data.images.webp.image_url}
                 order={i}
                 synopsis={data.synopsis}
                 genre={data.genres}
-                score ={data.score}
-                type ={data.type}
-                episodes = {data.episodes}
-                duration = {data.duration}
-                airing = {data.airing}
+                score={data.score}
+                type={data.type}
+                episodes={data.episodes}
+                duration={data.duration}
+                airing={data.airing}
               />
             </div>
           );
